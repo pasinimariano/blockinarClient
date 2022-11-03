b87ba77c90d2dfb2dfd42ff7f98daca195cffba2
pasinimariano <marianolpasini@juacaxdev.com>
2022-11-03 10:17:33 -0300

    .

--- _document.js
+++ _document.js
@@ -5,14 +5,14 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preload" href="https://fonts.googleapis.com" />
+          <link rel="preload" href="https://fonts.gstatic.com" crossorigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;1,100&display=swap"
             rel="stylesheet"
           />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preload" href="https://fonts.googleapis.com" />
+          <link rel="preload" href="https://fonts.gstatic.com" crossorigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;1,100;1,700&display=swap"
             rel="stylesheet"
